2b071742958a91e341045ae8f1756bbda929cd9a
Mindy Preston <meetup@yomimono.org>
2015-07-31 14:44:28 +0100

    separate file for module types

---
+++ lib/expiring_entry.mli
@@ -0,0 +1,22 @@
+module type C_S = sig
+  type t
+  val compare : t -> t -> int
+  val to_string : t -> string
+  val of_string : string -> t option
+end
+
+module type ENTRY = sig
+
+  type entry
+  type t = 
+    | Confirmed of float * entry
+
+  type result = [ `Ok of entry | `Timeout ]
+
+  val to_string : t -> string
+  val to_json : t -> Ezjsonm.value
+  val of_json : Ezjsonm.value -> t option
+  val compare : t -> t -> int
+  val make_confirmed : float -> entry -> t
+end
+
