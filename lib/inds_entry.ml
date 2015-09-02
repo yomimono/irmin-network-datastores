d0d1ae927f0029bae9c0b7fd351f75061adab0e0
Mindy Preston <meetup@yomimono.org>
2015-09-02 11:30:59 -0700

    conform to a more exceptional API.

--- lib/inds_entry.ml
+++ lib/inds_entry.ml
@@ -5,27 +5,25 @@ module Make(Entry: Inds_types.ENTRY_ELIGIBLE) = struct
   type t = 
     | Confirmed of int * Entry.t
 
-  let make_confirmed time mac = Confirmed (time, mac)
-
-  let to_string = function
-    | Confirmed (time, mac) -> Printf.sprintf "%s expiring at %d"
-                                 (Entry.to_string mac) time
+  let make_confirmed time entry = Confirmed (time, entry)
 
   let to_json = function
-    | Confirmed (time, mac) -> 
+    | Confirmed (time, entry) -> 
       let expiry = Ezjsonm.int time in
-      let mac = Ezjsonm.string (Entry.to_string mac) in
-      Ezjsonm.dict [("expiry", expiry); ("mac", mac)]
+      let entry = Ezjsonm.string (Entry.to_string entry) in
+      Ezjsonm.dict [("expiry", expiry); ("entry", entry)]
 
-  let of_json (json : Ezjsonm.value) : t option = match json with
-    | `A _ | `Null | `Bool _ | `Float _ | `String _ -> None
+  let of_json (json : Ezjsonm.value) = match json with
+    | `A _ | `Null | `Bool _ | `Float _ | `String _ ->
+      raise (Invalid_argument "entry.of_json expects a dictionary,
+      but was given some other json")
     | `O items ->
         let open Ezjsonm in
-          match (Entry.of_string (get_string (find (dict items) ["mac"]))) with
-          | None -> None
+          match (Entry.of_string (get_string (find (dict items) ["entry"]))) with
+          | None -> raise (Invalid_argument "parse failure in an entry")
           | Some address -> (
             let expiry = get_int (find (dict items) ["expiry"]) in
-            Some (Confirmed (expiry, address)) )
+            Confirmed (expiry, address))
 
   let compare p q =
     match (p, q) with
